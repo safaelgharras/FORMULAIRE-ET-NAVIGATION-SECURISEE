9d86085b68d075550a992cf4cf07bad0a539154c
safaelgharras <ehsafaa7@gmail.com>
2026-01-16 09:45:53 +0000

    file

---
+++ Chapitre 1-Récupération via $_GET, $_POST, validation simple/Traitement.php
@@ -0,0 +1,15 @@
+<?php
+if ($_SERVER["REQUEST METHOD"] === "POST"){
+    $nom = trim($_POST['nom']);
+    $email = trim($_POST['email']);
+
+if (empty($nom) || empty($email)){
+    echo "Tous les champs sont obligatoires.";
+}elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    echo "Email invalide";
+}else{
+    echo "Bonjour $nom, votre email est : $email";
+}
+
+} 
+ 
