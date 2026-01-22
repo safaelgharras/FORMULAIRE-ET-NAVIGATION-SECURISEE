2916b96df572cd1bd4a786c4eeba6284a629fdef
safaelgharras <ehsafaa7@gmail.com>
2026-01-22 09:16:36 +0000

    File

---
+++ Chapitre 3 – Démarrer une session utilisateur/login.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+
+
+if ($_SERVER["REQUEST_METHOD"] === "POST"){
+    $nom = trim($_POST['nom']);
+    if (!empty($nom)){
+        $_SESSION['utilisateur'] = $nom;
+        header('location: profile.php');
+        exit;
+}else{
+    $message = 'Veuillez entrer votre nom.';
+}
+}
+
+?>
+
+<form method="POST">
+    <label >Nom</label>
+    <input type="text" name="nom">
+    <button type="submit">LogIn</button>
+</form>
+
+<?php if (!empty($message)) echo "<p style='color:red;'>$message</p>"; ?>
+
+
