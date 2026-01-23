d296e8ba8957fade1cb34ed404315e63635e06db
safaelgharras <ehsafaa7@gmail.com>
2026-01-23 11:00:43 +0000

    File

--- Prototype 10/traitement.php
+++ Prototype 10/traitement.php
@@ -36,5 +36,67 @@ if (empty($nom) || empty($email) || empty($message)){
     <button type="submit">Envoyer</button>
     </form>
     
+
+    <style>
+    body {
+        font-family: Arial, sans-serif;
+        background-color: #f0f4ff; /* bleu clair */
+    }
+
+    form {
+        background-color: white;
+        width: 300px;
+        margin: 60px auto;
+        padding: 20px;
+        border-radius: 6px;
+    }
+
+    label {
+        font-weight: bold;
+        color: #003366;
+    }
+
+    input, textarea {
+        width: 100%;
+        padding: 8px;
+        border: 2px solid #ccc;
+        border-radius: 4px;
+        transition: all 0.3s ease;
+    }
+
+    /* Quand on clique dans l’input */
+    input:focus,
+    textarea:focus {
+        transform: scale(3.7);
+        max-width:100%;
+        border-color: #003366;
+        box-shadow: 0 0 6px rgba(0, 51, 102, 0.5);
+        outline: none;
+    }
+
+
+    button {
+        width: 100%;
+        padding: 10px;
+        background-color: #003366;
+        color: white;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        margin-top: 10px;
+    }
+
+    button:hover {
+        opacity: 0.9;
+    }
+
+    p {
+        text-align: center;
+        font-weight: bold;
+        color: #003366;
+    }
+</style>
+
+
 </body>
 </html>
